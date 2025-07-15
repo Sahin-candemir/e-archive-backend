f18f307324e71d26815a96526619e6013141efbe
shnca <shncandemir@gmail.com>
2025-07-16 00:16:39 +0300

    fix cors

--- archiving-system/src/main/java/com/sahin/archiving_system/config/SecurityConfig.java
+++ archiving-system/src/main/java/com/sahin/archiving_system/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://e-archive-frontend.onrender.com"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
